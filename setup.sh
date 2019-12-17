1ffada8750d05e6019ae7c31de9904162d174935
himarjin <h.tada0817@gmail.com>
2019-12-17 20:00:21 +0900

    add plistings.sty

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ git clone https://github.com/takago/pd_report.git
 docker-compose up -d
 docker-compose exec tex cp /workdir/.latexmkrc /root
 docker-compose exec tex mkdir -p /usr/local/texlive/2019/texmf-dist/tex/latex/platex/base
+docker-compose exec tex wget http://zeus.info.kanazawa-it.ac.jp/~takago/mint/plistings.sty -O /usr/local/texlive/2019/texmf-dist/tex/latex/platex/base/plistings.sty
+docker-compose exec tex mktexlsr
